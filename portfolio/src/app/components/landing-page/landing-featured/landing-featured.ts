5332b2b84e37eb71561b564b65617b51bafc9c1d
feralsyntax <fullstack.benie@gmail.com>
2026-08-13 15:33:27 +0300

    add (components): generate landing page featured component

---
+++ portfolio/src/app/components/landing-page/landing-featured/landing-featured.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-featured',
+  imports: [],
+  templateUrl: './landing-featured.html',
+  styleUrl: './landing-featured.scss',
+})
+export class LandingFeatured {}
