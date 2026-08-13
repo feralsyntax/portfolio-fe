d26a6794f33477a6242412acc1440e8a4e818729
feralsyntax <fullstack.benie@gmail.com>
2026-08-13 15:51:26 +0300

    add (landing-page): next landing page exploring component

--- portfolio/src/app/components/landing-page/landing-page.ts
+++ portfolio/src/app/components/landing-page/landing-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LandingHero } from './landing-hero/landing-hero';
 import { LandingFeatured } from './landing-featured/landing-featured';
+import { LandingExploring } from './landing-exploring/landing-exploring';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [LandingHero, LandingFeatured],
+  imports: [LandingHero, LandingFeatured, LandingExploring],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
 })
