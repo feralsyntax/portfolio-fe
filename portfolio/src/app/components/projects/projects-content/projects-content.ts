3567783b5d146038ce04c24f840473cca07a4433
feralsyntax <fullstack.benie@gmail.com>
2026-08-13 18:45:07 +0300

    add (components): generate projects content component

---
+++ portfolio/src/app/components/projects/projects-content/projects-content.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects-content',
+  imports: [],
+  templateUrl: './projects-content.html',
+  styleUrl: './projects-content.scss',
+})
+export class ProjectsContent {}
