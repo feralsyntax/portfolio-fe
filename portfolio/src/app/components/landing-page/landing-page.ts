140996995546731a088ccde2fbb34f6a418755ab
feralsyntax <fullstack.benie@gmail.com>
2026-08-13 15:58:49 +0300

    add (landing-page): nest footer

--- portfolio/src/app/components/landing-page/landing-page.ts
+++ portfolio/src/app/components/landing-page/landing-page.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { LandingHero } from './landing-hero/landing-hero';
 import { LandingFeatured } from './landing-featured/landing-featured';
 import { LandingExploring } from './landing-exploring/landing-exploring';
+import { Footer } from '../navigation/footer/footer';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [LandingHero, LandingFeatured, LandingExploring],
+  imports: [LandingHero, LandingFeatured, LandingExploring, Footer],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
 })
