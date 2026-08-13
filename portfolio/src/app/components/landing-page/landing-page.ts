71cac35d1c6c81f0842e775a35e62c20fac36b02
feralsyntax <fullstack.benie@gmail.com>
2026-08-13 15:43:46 +0300

    add (landing-page): nest landing page featured component

--- portfolio/src/app/components/landing-page/landing-page.ts
+++ portfolio/src/app/components/landing-page/landing-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LandingHero } from './landing-hero/landing-hero';
+import { LandingFeatured } from './landing-featured/landing-featured';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [LandingHero],
+  imports: [LandingHero, LandingFeatured],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
 })
