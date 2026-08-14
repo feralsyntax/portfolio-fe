a52837eead583610a2dfa69d6b28bbf939ea8317
feralsyntax <fullstack.benie@gmail.com>
2026-08-14 10:25:00 +0300

    add (routes): configure a route for the projects component

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { LandingPage } from './components/landing-page/landing-page';
+import { Projects } from './components/projects/projects';
 
-export const routes: Routes = [{ path: '', component: LandingPage, title: 'Welcome' }];
+export const routes: Routes = [
+  { path: '', component: LandingPage, title: 'Welcome' },
+  { path: 'projects', component: Projects, title: 'All Projects' },
+];
