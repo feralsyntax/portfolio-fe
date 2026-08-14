59d229234c4f895ca398ecaabbd186503b4ac11a
feralsyntax <fullstack.benie@gmail.com>
2026-08-14 10:10:46 +0300

    add (components, projects): generate projects industries component

---
+++ portfolio/src/app/components/projects/projects-industries/projects-industries.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects-industries',
+  imports: [],
+  templateUrl: './projects-industries.html',
+  styleUrl: './projects-industries.scss',
+})
+export class ProjectsIndustries {}
