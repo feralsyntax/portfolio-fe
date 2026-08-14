150fe702c9eeff46d3930ec69535bd8fbe9abeab
feralsyntax <fullstack.benie@gmail.com>
2026-08-14 11:33:09 +0300

    add (p-details-content): add component logic

--- portfolio/src/app/components/project-details/p-details-content/p-details-content.ts
+++ portfolio/src/app/components/project-details/p-details-content/p-details-content.ts
@@ -6,4 +6,25 @@ import { Component } from '@angular/core';
   templateUrl: './p-details-content.html',
   styleUrl: './p-details-content.scss',
 })
-export class PDetailsContent {}
+export class PDetailsContent {
+  frontendTechnologies = ['Angular', 'TypeScript'];
+
+  backendTechnologies = ['Django', 'Python', 'PostgreSQL'];
+
+  otherTechnologies = ['Docker', 'AWS'];
+
+  features = [
+    {
+      name: 'Workflow',
+      description: 'Maker-checker approval flow for data integrity.',
+    },
+    {
+      name: 'Tracking',
+      description: 'Real-time inventory and sales monitoring.',
+    },
+    {
+      name: 'Operations',
+      description: 'Automated shift handovers and incident logging.',
+    },
+  ];
+}
