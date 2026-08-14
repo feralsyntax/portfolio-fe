e0c6f3025a733404216c37fa311e2537fa417f08
feralsyntax <fullstack.benie@gmail.com>
2026-08-14 16:47:36 +0300

    add (contact): nest children components

--- portfolio/src/app/components/contact/contact.ts
+++ portfolio/src/app/components/contact/contact.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { ContactIntro } from './contact-intro/contact-intro';
+import { ContactForm } from './contact-form/contact-form';
+import { ContactSkills } from './contact-skills/contact-skills';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ContactIntro, ContactForm, ContactSkills],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
