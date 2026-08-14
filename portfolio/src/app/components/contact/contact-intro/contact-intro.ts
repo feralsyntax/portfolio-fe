be027f39ba9cbce2ff118ae5d1a03d71bf31e4fd
feralsyntax <fullstack.benie@gmail.com>
2026-08-14 16:35:50 +0300

    add (contact): generate contact intro component

---
+++ portfolio/src/app/components/contact/contact-intro/contact-intro.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-intro',
+  imports: [],
+  templateUrl: './contact-intro.html',
+  styleUrl: './contact-intro.scss',
+})
+export class ContactIntro {}
