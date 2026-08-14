dc0abbe6277dd1bc7102efc6b00bf3d71f681ace
feralsyntax <fullstack.benie@gmail.com>
2026-08-14 16:49:07 +0300

    add (routes): configure a route for the contact component

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LandingPage } from './components/landing-page/landing-page';
 import { Projects } from './components/projects/projects';
 import { ProjectDetails } from './components/project-details/project-details';
 import { About } from './components/about/about';
+import { Contact } from './components/contact/contact';
 
 export const routes: Routes = [
   { path: '', component: LandingPage, title: 'Welcome' },
   { path: 'projects', component: Projects, title: 'All Projects' },
   { path: 'p/:uuid', component: ProjectDetails, title: 'Project Details' },
   { path: 'about', component: About, title: 'My Journey' },
+  { path: 'contact', component: Contact, title: "Let's Connect" },
 ];
