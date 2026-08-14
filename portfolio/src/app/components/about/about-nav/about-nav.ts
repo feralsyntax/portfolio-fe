3970728b8e5b5468303a44ceaf292b8238506069
feralsyntax <fullstack.benie@gmail.com>
2026-08-14 13:47:23 +0300

    add (about): generate about navbar component

---
+++ portfolio/src/app/components/about/about-nav/about-nav.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-nav',
+  imports: [],
+  templateUrl: './about-nav.html',
+  styleUrl: './about-nav.scss',
+})
+export class AboutNav {}
