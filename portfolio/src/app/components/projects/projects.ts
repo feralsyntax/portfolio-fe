12f06e52d68fe8812efe7d30d826f81153e7ae10
feralsyntax <fullstack.benie@gmail.com>
2026-08-14 10:23:20 +0300

    add (projects): nest components

--- portfolio/src/app/components/projects/projects.ts
+++ portfolio/src/app/components/projects/projects.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { ProjectsPageContext } from './projects-page-context/projects-page-context';
+import { ProjectsContent } from './projects-content/projects-content';
+import { ProjectsFeatures } from './projects-features/projects-features';
+import { ProjectsTechnologies } from './projects-technologies/projects-technologies';
+import { ProjectsIndustries } from './projects-industries/projects-industries';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [
+    ProjectsPageContext,
+    ProjectsContent,
+    ProjectsTechnologies,
+    ProjectsFeatures,
+    ProjectsIndustries,
+  ],
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
 })
