4fbac2f76b7ce3eb9d21f58ff0d80baac734ec88
feralsyntax <fullstack.benie@gmail.com>
2026-08-14 14:29:22 +0300

    add (about): generate about exploring component

---
+++ portfolio/src/app/components/about/about-exploring/about-exploring.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-exploring',
+  imports: [],
+  templateUrl: './about-exploring.html',
+  styleUrl: './about-exploring.scss',
+})
+export class AboutExploring {}
