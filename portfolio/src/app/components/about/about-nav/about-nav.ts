7f4126fbb6b2fc7f15ce09611aa0347475d1c1ae
feralsyntax <fullstack.benie@gmail.com>
2026-08-14 15:50:36 +0300

    change (components): restructure & restyle components for the about page

--- portfolio/src/app/components/about/about-nav/about-nav.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-about-nav',
-  imports: [],
-  templateUrl: './about-nav.html',
-  styleUrl: './about-nav.scss',
-})
-export class AboutNav {}
--- portfolio/src/app/components/about/about.ts
+++ portfolio/src/app/components/about/about.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { AboutHero } from './about-hero/about-hero';
+import { AboutTimeline } from './about-timeline/about-timeline';
+import { AboutExploring } from './about-exploring/about-exploring';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [AboutHero, AboutTimeline, AboutExploring],
   templateUrl: './about.html',
   styleUrl: './about.scss',
 })
