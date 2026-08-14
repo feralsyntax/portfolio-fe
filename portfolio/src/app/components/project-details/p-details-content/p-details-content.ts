84c3d973454139e680a6d118fc0a7a4f68e18027
feralsyntax <fullstack.benie@gmail.com>
2026-08-14 11:31:40 +0300

    add (project-details): generate project details content component

---
+++ portfolio/src/app/components/project-details/p-details-content/p-details-content.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-p-details-content',
+  imports: [],
+  templateUrl: './p-details-content.html',
+  styleUrl: './p-details-content.scss',
+})
+export class PDetailsContent {}
