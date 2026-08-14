2efd8b61dc3b17ebf83120ea65996603695bb58b
feralsyntax <fullstack.benie@gmail.com>
2026-08-14 15:51:24 +0300

    add (routes): configure a route for the about component

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LandingPage } from './components/landing-page/landing-page';
 import { Projects } from './components/projects/projects';
 import { ProjectDetails } from './components/project-details/project-details';
+import { About } from './components/about/about';
 
 export const routes: Routes = [
   { path: '', component: LandingPage, title: 'Welcome' },
   { path: 'projects', component: Projects, title: 'All Projects' },
   { path: 'p/:uuid', component: ProjectDetails, title: 'Project Details' },
+  { path: 'about', component: About, title: 'My Journey' },
 ];
