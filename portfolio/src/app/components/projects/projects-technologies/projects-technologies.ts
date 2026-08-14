f70c1abd54577f8edc5966043426b550dd2804c4
feralsyntax <fullstack.benie@gmail.com>
2026-08-14 09:37:32 +0300

    add (components, projects): generate projects technologies component

---
+++ portfolio/src/app/components/projects/projects-technologies/projects-technologies.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects-technologies',
+  imports: [],
+  templateUrl: './projects-technologies.html',
+  styleUrl: './projects-technologies.scss',
+})
+export class ProjectsTechnologies {}
