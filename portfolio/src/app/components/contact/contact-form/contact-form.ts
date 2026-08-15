af5f215ce8dfcdbf941b8ff01d2a1cc881f859bc
feralsyntax <fullstack.benie@gmail.com>
2026-08-15 11:21:19 +0300

    update (footer): add socials urls && change (contact): update github spelling

--- portfolio/src/app/components/contact/contact-form/contact-form.ts
+++ portfolio/src/app/components/contact/contact-form/contact-form.ts
@@ -12,7 +12,7 @@ export class ContactForm {
   readonly contactTimezone = 'EAT / UTC+3';
   readonly contactResponseTime = 'Usually within 24 hours.';
 
-  readonly contactGithubUrl = 'https://github.com/feralsyntax';
+  readonly contactGitHubUrl = 'https://github.com/feralsyntax';
   readonly contactLinkedInUrl = 'https://ke.linkedin.com/in/benson-langat-software-developer';
   readonly contactPhoneNumber = '+254708696335';
 }
