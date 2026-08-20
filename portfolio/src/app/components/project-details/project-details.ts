5b5d074cbb65f33abf4e3d110a88c67e41fe159b
feralsyntax <fullstack.benie@gmail.com>
2026-08-20 11:49:04 +0300

    add (project-details): add component logic

--- portfolio/src/app/components/project-details/project-details.ts
+++ portfolio/src/app/components/project-details/project-details.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { PDetailsNav } from './p-details-nav/p-details-nav';
 import { PDetailsTitle } from './p-details-title/p-details-title';
 import { PDetailsSnapshot } from './p-details-snapshot/p-details-snapshot';
 import { PDetailsDescription } from './p-details-description/p-details-description';
 import { PDetailsContent } from './p-details-content/p-details-content';
 import { PDetailsMore } from './p-details-more/p-details-more';
+import { ActivatedRoute } from '@angular/router';
+import { ProjectsDataService } from '../../services/projects-data/projects-data-service';
 
 @Component({
   selector: 'app-project-details',
@@ -19,4 +21,11 @@ import { PDetailsMore } from './p-details-more/p-details-more';
   templateUrl: './project-details.html',
   styleUrl: './project-details.scss',
 })
-export class ProjectDetails {}
+export class ProjectDetails {
+  private readonly route = inject(ActivatedRoute);
+  protected readonly projectsData = inject(ProjectsDataService);
+
+  protected readonly uuid = this.route.snapshot.paramMap.get('uuid')!;
+
+  protected readonly project = this.projectsData.getProjectByUuid(this.uuid);
+}
