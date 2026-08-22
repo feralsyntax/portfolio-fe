6b244b419902051e7068386421973adc05b9bc20
feralsyntax <fullstack.benie@gmail.com>
2026-08-22 16:07:57 +0300

    add (routes): configure a route for the 404 component

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Projects } from './components/projects/projects';
 import { ProjectDetails } from './components/project-details/project-details';
 import { About } from './components/about/about';
 import { Contact } from './components/contact/contact';
+import { FourOhFour } from './components/four-oh-four/four-oh-four';
 
 export const routes: Routes = [
   { path: '', component: LandingPage, title: 'Welcome' },
@@ -11,4 +12,5 @@ export const routes: Routes = [
   { path: 'p/:uuid', component: ProjectDetails, title: 'Project Details' },
   { path: 'about', component: About, title: 'My Journey' },
   { path: 'contact', component: Contact, title: "Let's Connect" },
+  { path: '**', component: FourOhFour, title: 'Page Not Found' },
 ];
