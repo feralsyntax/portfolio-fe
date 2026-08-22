40c4590ce662a3708d82977f6e72fccf34908291
feralsyntax <fullstack.benie@gmail.com>
2026-08-22 15:10:05 +0300

    update (routes): add more routes whose render mode is client

--- portfolio/src/app/app.routes.server.ts
+++ portfolio/src/app/app.routes.server.ts
@@ -6,6 +6,14 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Prerender,
   },
   {
+    path: '',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'projects',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: 'p/:uuid',
     renderMode: RenderMode.Client,
   },
