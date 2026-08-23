288bd2d93f1761f1bd63741e37af0c783004f156
feralsyntax <fullstack.benie@gmail.com>
2026-08-23 12:00:04 +0300

    update (landing-featured): truncate long description

--- portfolio/src/app/components/landing-page/landing-featured/landing-featured.ts
+++ portfolio/src/app/components/landing-page/landing-featured/landing-featured.ts
@@ -1,10 +1,11 @@
 import { Component, computed, inject } from '@angular/core';
 import { ProjectsDataService } from '../../../services/projects-data/projects-data-service';
 import { Router } from '@angular/router';
+import { TruncatePipe } from '../../../pipes/truncate/truncate-pipe';
 
 @Component({
   selector: 'app-landing-featured',
-  imports: [],
+  imports: [TruncatePipe],
   templateUrl: './landing-featured.html',
   styleUrl: './landing-featured.scss',
 })
