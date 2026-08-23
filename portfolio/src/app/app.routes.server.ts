57fd2317e1405c4923242c9c47ebd68432d5c551
feralsyntax <fullstack.benie@gmail.com>
2026-08-23 17:13:18 +0300

    change (routes): change the default render mode to client

--- portfolio/src/app/app.routes.server.ts
+++ portfolio/src/app/app.routes.server.ts
@@ -2,19 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'projects',
-    renderMode: RenderMode.Client,
-  },
-  {
-    path: 'p/:uuid',
-    renderMode: RenderMode.Client,
-  },
-  {
-    path: 'contact',
-    renderMode: RenderMode.Client,
-  },
-  {
     path: '**',
-    renderMode: RenderMode.Prerender,
+    renderMode: RenderMode.Client,
   },
 ];
