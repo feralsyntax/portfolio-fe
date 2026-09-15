7e158e52de8137b068766471127f93238e2d3dbf
feralsyntax <fullstack.benie@gmail.com>
2026-09-15 16:32:25 +0300

    add (p-details-more): add routing to more projects && update: track uuid instead of name

--- portfolio/src/app/components/project-details/p-details-more/p-details-more.ts
+++ portfolio/src/app/components/project-details/p-details-more/p-details-more.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { Project } from '../../../api/openapi';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-p-details-more',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './p-details-more.html',
   styleUrl: './p-details-more.scss',
 })
